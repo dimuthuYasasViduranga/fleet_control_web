8d3657c11f8cb8770a9ff97a25512a5f442b49c2
Jon <jonathan.howell@haultrax.com>
2021-04-19 13:07:28 +0800

    added control categories to operator state

--- phx/lib/dispatch_web/channels/operator/operator_channel.ex
+++ phx/lib/dispatch_web/channels/operator/operator_channel.ex
@@ -400,6 +400,7 @@ defmodule DispatchWeb.OperatorChannel do
       track: latest_track,
       other_tracks: other_tracks,
       pre_start_ticket_status_types: PreStartSubmissionAgent.ticket_status_types(),
+      pre_start_control_categories: PreStartAgent.categories(),
       pre_start_forms: PreStartAgent.all(),
       pre_start_submissions: %{
         current: PreStartSubmissionAgent.current(asset_id),
