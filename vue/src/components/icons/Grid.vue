f046a58721a81eeb62fa1b09ff2831909ddad3eb
Jon <jonathan.howell@haultrax.com>
2021-05-20 13:09:52 +0800

    Added asset overview page placeholder

---
+++ vue/src/components/icons/Grid.vue
@@ -0,0 +1,13 @@
+<template>
+  <g id="grid_icon">
+    <path stroke-linejoin="round" d="M1.5 1.5h8v8h-8zm11 0h8v8h-8zm-11 11h8v8h-8zm11 0h8v8h-8z" />
+  </g>
+</template>
+
+<script>
+export default {
+  name: 'GridIcon',
+  size: 22,
+  pxIconSet: 'px-nav:grid-view',
+};
+</script>
---
+++ vue/src/components/pages/asset_overview/AssetOverviewPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="asset-overview-page">
+    This is some text
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AssetOverviewPage',
+}
+</script>
+
+<style>
+</style>
