7e36e3af3f7f4b06b412b8f52a9d0a5359f9f01b
Jon <jonathan.howell@haultrax.com>
2021-05-20 13:27:37 +0800

    Ability to toggle fullscreen on component

---
+++ vue/src/components/icons/Fullscreen.vue
@@ -0,0 +1,15 @@
+<template>
+  <g id="fullscreen">
+    <path stroke-linejoin="round" d="M4 18L18 4m-8-.5h8.5V12M12 18.5H3.5V10" />
+  </g>
+</template>
+
+<script>
+// outer rectangle removed
+export default {
+  name: 'FullscreenIcon',
+  size: 22,
+  pxIconSet: 'px-vis:full-screen',
+};
+</script>
+
--- vue/src/components/pages/asset_overview/AssetOverviewPage.vue
+++ vue/src/components/pages/asset_overview/AssetOverviewPage.vue
@@ -1,14 +1,69 @@
 <template>
-  <div class="asset-overview-page">
-    This is some text
-  </div>
+  <hxCard ref="page" class="asset-overview-page" :hideTitle="true">
+    <Icon class="fullscreen-toggle" :icon="fullscreenIcon" @click="toggleFullscreen()" />
+    <div class="controls">Controls go here</div>
+    <div class="asset-tiles">
+      <div>Apple</div>
+      <div>Banans</div>
+    </div>
+  </hxCard>
 </template>
 
 <script>
+import hxCard from 'hx-layout/Card.vue';
+import Icon from 'hx-layout/Icon.vue';
+
+import { isElementFullscreen, requestFullscreen, exitFullscreen } from '@/code/fullscreen';
+
+import FullscreenIcon from '@/components/icons/Fullscreen.vue';
+
 export default {
   name: 'AssetOverviewPage',
-}
+  components: {
+    hxCard,
+    Icon,
+  },
+  data: () => {
+    return {
+      fullscreenIcon: FullscreenIcon,
+      isFullscreen: false,
+    };
+  },
+  mounted() {
+    document.addEventListener('fullscreenchange', this.onFullscreenChange, false);
+  },
+  beforeDestroy() {
+    document.removeEventListener('fullscreenchange', this.onFullscreenChange);
+  },
+  methods: {
+    toggleFullscreen() {
+      const page = this.$refs.page;
+
+      if (!page) {
+        console.error('[AssetOverview] Unable to enter fullscreen');
+        return;
+      }
+
+      const pageEl = page.$el;
+      this.isFullscreen ? exitFullscreen(pageEl) : requestFullscreen(pageEl);
+    },
+    onFullscreenChange() {
+      this.isFullscreen = isElementFullscreen();
+    },
+  },
+};
 </script>
 
 <style>
+.asset-overview-page .fullscreen-toggle {
+  cursor: pointer;
+  position: absolute;
+  right: 0;
+  margin-top: 1rem;
+  margin-right: 1rem;
+}
+
+.asset-overview-page .fullscreen-toggle:hover {
+  opacity: 0.5;
+}
 </style>
