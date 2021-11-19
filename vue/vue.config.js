f37e505c6acc61e3ecabfacdface824720de73e9
Jon <jonathan.howell@haultrax.com>
2021-11-19 17:44:56 +0800

    🔨 added conditional analysis of node_modules through yarn

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,12 +1,18 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
-  configureWebpack: {
-    plugins: [new BundleAnalyzerPlugin()],
-    externals: {
-      canvg: 'canvg',
-      html2canvas: 'html2canvas',
-      dompurify: 'dompurify',
-    },
+const webpackConfig = {
+  externals: {
+    canvg: 'canvg',
+    html2canvas: 'html2canvas',
+    dompurify: 'dompurify',
   },
+};
+
+if (process.env.NODE_ENV === 'node_modules') {
+  console.log('\n[Analyze] Node modules will be analysed and display along with the serve\n')
+  webpackConfig.plugins = [new BundleAnalyzerPlugin()];
+}
+
+module.exports = {
+  configureWebpack: webpackConfig,
 };
