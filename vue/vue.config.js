d79a44ae1361666fed81e0a8de84665073259a89
Jon <jonathan.howell@haultrax.com>
2021-11-19 16:03:39 +0800

    reduced some more package size

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -3,5 +3,10 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 module.exports = {
   configureWebpack: {
     plugins: [new BundleAnalyzerPlugin()],
+    externals: {
+      canvg: 'canvg',
+      html2canvas: 'html2canvas',
+      dompurify: 'dompurify',
+    },
   },
 };
