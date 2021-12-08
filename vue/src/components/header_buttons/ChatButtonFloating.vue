1c05f4e35e48088a0ebfc7d5ae624825455687ac
Jon <jonathan.howell@haultrax.com>
2021-12-08 10:30:10 +0800

    Added the ability to move the chat head around (after long click)

--- vue/src/components/header_buttons/ChatButtonFloating.vue
+++ vue/src/components/header_buttons/ChatButtonFloating.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="chat-button-floating">
-    <div class="outer-circle-wrapper" @click="open">
+  <div
+    class="chat-button-floating"
+    :style="`${transform}`"
+    @mousedown="onMouseDown"
+    @mouseup="onMouseUp"
+  >
+    <div class="outer-circle-wrapper">
       <div class="outer-circle">
         <ChatButton :clickable="false" />
       </div>
@@ -11,10 +16,60 @@
 <script>
 import ChatButton from './ChatButton.vue';
 
+const OFFSET = 37;
+const LONG_PRESS_PERIOD = 500;
+
+function getPosition(event, width, height) {
+  const position = {
+    top: null,
+    bottom: null,
+    left: null,
+    right: null,
+  };
+  const { x, y } = event;
+
+  if (x < width / 2) {
+    position.left = x;
+  } else {
+    position.right = width - x - OFFSET;
+  }
+
+  if (y < height / 2) {
+    position.top = y;
+  } else {
+    position.bottom = height - y - OFFSET;
+  }
+
+  return position;
+}
+
 export default {
   name: 'ChatButtonFloating',
   components: {
     ChatButton,
+  },
+  data: () => {
+    return {
+      mouseDownTimeout: null,
+      moved: false,
+      width: 0,
+      height: 0,
+      top: null,
+      left: null,
+      right: 50,
+      bottom: 75,
+    };
+  },
+  computed: {
+    transform() {
+      const vertical = this.top == null ? `bottom: ${this.bottom}px` : `top: ${this.top}px`;
+      const horizontal = this.left == null ? `right: ${this.right}px` : `left: ${this.left}px`;
+      return `${vertical}; ${horizontal};`;
+    },
+  },
+  beforeDestroy() {
+    this.clearMouseDownTimeout();
+    this.clearListeners();
   },
   methods: {
     open() {
@@ -23,6 +78,49 @@ export default {
         assetId: 'all',
       };
       this.$eventBus.$emit('chat-open', options);
+    },
+    onMouseDown() {
+      this.moved = false;
+      this.setMouseDownTimeout();
+    },
+    setMouseDownTimeout() {
+      this.clearMouseDownTimeout();
+      this.mouseDownTimeout = setTimeout(() => {
+        this.height = window.innerHeight;
+        this.width = window.innerWidth;
+        this.setListeners();
+      }, LONG_PRESS_PERIOD);
+    },
+    setListeners() {
+      document.addEventListener('mouseup', this.onMouseUp, false);
+      document.addEventListener('mousemove', this.onMouseMove, false);
+    },
+    clearListeners() {
+      document.removeEventListener('mousemove', this.onMouseMove);
+      document.removeEventListener('mouseup', this.onMouseUp);
+    },
+    clearMouseDownTimeout() {
+      clearTimeout(this.mouseDownTimeout);
+    },
+    onMouseMove(event) {
+      console.log('move');
+
+      this.moved = true;
+      const { top, bottom, left, right } = getPosition(event, this.width, this.height);
+      this.top = top;
+      this.bottom = bottom;
+      this.left = left;
+      this.right = right;
+    },
+    onMouseUp() {
+      this.clearMouseDownTimeout();
+      this.clearListeners();
+
+      if (!this.moved) {
+        this.open();
+      }
+
+      this.moved = false;
     },
   },
 };
@@ -33,8 +131,6 @@ export default {
   position: fixed;
   height: 75px;
   width: 75px;
-  bottom: 65px;
-  right: 50px;
   z-index: 10;
 }
 
