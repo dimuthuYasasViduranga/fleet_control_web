2e298a2584fbc55f68a8ca619269425d471977c8
tyson <tyson.buzza@gmail.com>
2022-06-17 17:19:13 +0800

    standardise config

--- phx/config/prod.exs
+++ phx/config/prod.exs
@@ -1,16 +1,6 @@
 import Config
 
-random_string = fn size ->
-  size
-  |> :crypto.strong_rand_bytes()
-  |> Base.encode64()
-  |> binary_part(0, size)
-end
-
-config :dispatch_web, bypass_auth: false
-
 config :dispatch_web, DispatchWeb.Endpoint,
-  secret_key_base: random_string.(64),
   load_from_system_env: true,
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
@@ -18,12 +8,11 @@ config :dispatch_web, DispatchWeb.Endpoint,
   version: Application.spec(:phoenix_app, :vsn),
   http: [port: 4001]
 
-config :dispatch_web, DispatchWeb.Guardian, secret_key: random_string.(64)
-
 # Do not print debug messages in production
 config :logger,
   level: :info,
   compile_time_purge_matching: [
+    [application: :gps_gate_rest, level_lower_than: :info],
     [application: :db_connection],
     [module: DBConnection.Connection]
   ]
