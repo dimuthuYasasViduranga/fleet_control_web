81c6bed04ac6342b0321d600ebca42e4dad61a1f
tyson-laptop <tyson.buzza@gmail.com>
2022-06-27 14:33:56 +0800

    rename files from dispatch to fleet_control

--- phx/mix.exs
+++ phx/mix.exs
@@ -3,7 +3,7 @@ defmodule FleetControlWeb.MixProject do
 
   def project do
     [
-      app: :dispatch_web,
+      app: :fleet_control_web,
       version: "0.1.0",
       build_path: "_build",
       config_path: "config/config.exs",
