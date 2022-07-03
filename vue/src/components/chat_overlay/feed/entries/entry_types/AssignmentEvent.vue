fb75b7a5fcec03a0cc6dd3af76dd2295a0a44b1e
MSS <mandeep.sandhu@haultrax.com>
2022-07-03 22:01:43 +0800

    Add click to DM support for Assignment Event in live feed.

--- vue/src/components/chat_overlay/feed/entries/entry_types/AssignmentEvent.vue
+++ vue/src/components/chat_overlay/feed/entries/entry_types/AssignmentEvent.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="assignment-event">
-    <div class="title">{{ entry.assetName }} | {{ action }}</div>
+    <div class="title">
+      <a href="entry.assetName"  @click.prevent="dmAsset(entry.assetId)">{{ entry.assetName }}</a> 
+      <span> | {{ action }}</span>
+    </div>
   </div>
 </template>
 
@@ -21,6 +24,17 @@ export default {
       return `Device '${uuid}' removed`;
     },
   },
+  methods: {
+    dmAsset(assetId) {
+
+      const opts = {
+        scroll: 'bottom',
+        assetId: assetId 
+      };
+
+      this.$eventBus.$emit('chat-open', opts);
+    }
+  }
 };
 </script>
 
@@ -30,5 +44,10 @@ export default {
   padding: 0.25rem;
   background-color: #2e2836;
   color: darkgray;
+  white-space: pre;
+}
+
+.assignment-event a { 
+   color: darkgray;
 }
 </style>
