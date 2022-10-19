35b22f8d93f3e75bf83ba203762886e4579942e7
machakos23 <mandeep.sandhu@haultrax.com>
2022-10-19 11:23:14 +0800

    rolling back use_pre_starts flag changes

--- phx/config/config.exs
+++ phx/config/config.exs
@@ -20,6 +20,7 @@ config :fleet_control_web,
   track_method: :gps_gate,
   settings: [
     use_device_gps: false,
+    use_pre_starts: true,
     prompt_exception_on_logout: true,
     prompt_engine_hours_on_login: false,
     prompt_engine_hours_on_logout: false,
