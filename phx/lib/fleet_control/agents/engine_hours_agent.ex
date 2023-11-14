951ebb42d4903d4b2a0f132a8201ee575860cf09
whossname <tyson.buzza@gmail.com>
2023-11-14 13:00:56 +0800

    improve engine hous agent performance
    mostly impacts dispatcher report

--- phx/lib/fleet_control/agents/engine_hours_agent.ex
+++ phx/lib/fleet_control/agents/engine_hours_agent.ex
@@ -6,7 +6,6 @@ defmodule FleetControl.EngineHoursAgent do
   require Logger
   alias HpsData.Schemas.Dispatch.EngineHours
   alias HpsData.Repo
-  alias HpsData.Asset
 
   @cull_opts %{
     time_key: :timestamp,
@@ -168,52 +167,34 @@ defmodule FleetControl.EngineHoursAgent do
   end
 
   defp get_latest_query() do
-    from(a in Asset,
+    from(eh in EngineHours,
+      group_by: eh.asset_id,
+      where: eh.deleted != true,
       select: %{
-        asset_id: a.id,
-        timestamp:
-          fragment(
-            "SELECT
-              MAX(timestamp)
-            FROM dis_engine_hours
-            WHERE asset_id = ? and deleted != true",
-            a.id
-          )
+        asset_id: eh.asset_id,
+        timestamp: max(eh.timestamp)
       }
     )
   end
 
-  # though string interpolation would be nicer, this is the NON-sql injection way of doing it
   defp get_search_query(timestamp, ">") do
-    from(a in Asset,
+    from(eh in EngineHours,
+      group_by: eh.asset_id,
+      where: eh.timestamp > ^timestamp and eh.deleted != true,
       select: %{
-        asset_id: a.id,
-        timestamp:
-          fragment(
-            "SELECT
-              MIN(timestamp)
-            FROM dis_engine_hours
-            WHERE asset_id = ? and timestamp > ? and deleted != true",
-            a.id,
-            ^timestamp
-          )
+        asset_id: eh.asset_id,
+        timestamp: min(eh.timestamp)
       }
     )
   end
 
   defp get_search_query(timestamp, "<") do
-    from(a in Asset,
+    from(eh in EngineHours,
+      group_by: eh.asset_id,
+      where: eh.timestamp < ^timestamp and eh.deleted != true,
       select: %{
-        asset_id: a.id,
-        timestamp:
-          fragment(
-            "SELECT
-              MAX(timestamp)
-            FROM dis_engine_hours
-            WHERE asset_id = ? and timestamp < ? and deleted != true",
-            a.id,
-            ^timestamp
-          )
+        asset_id: eh.asset_id,
+        timestamp: max(eh.timestamp)
       }
     )
   end
